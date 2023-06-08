fd084bb0c0ec40da01d4622bc490b7fb0189ece1
tttom3669 <tttom3669@gmail.com>
2023-06-08 12:29:50 +0800

    update: 篩選按鈕

--- js/all.js
+++ js/all.js
@@ -3,6 +3,30 @@ $(function () {
     $(`[data-id="navbar-collapse"]`).fadeToggle();
   });
 
+  // 開起選單
+$(`[data-id="dropdown-btn"]`).click(function(e) {
+    $(`[data-id="dropdown-menu"]`).toggleClass('d-block');
+  });
+  $(`[data-id="filter-btn"]`).click(function(e) {
+    $(`[data-id="filter-dropdown-menu"]`).toggleClass('d-block');
+  });
+
+  
+  // 切換按鈕文字
+  $('.new-to-old').click(function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('d-block');
+    $('.dropdown-btnText').text($(this).text());
+  });
+  
+  $('.old-to-new').click(function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('d-block');
+    $('.dropdown-btnText').text($(this).text());
+  });
+
+
+
   $('.qa-item').click(function(e) {
     $(this).toggleClass('active');
     $(this).find('.add-icon').toggleClass('d-none');
